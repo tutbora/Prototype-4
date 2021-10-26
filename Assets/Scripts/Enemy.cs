42f9f12e4d2ec53eaa5f63279d5b498bf8e3f83d
tutbora <tutbora@gmail.com>
2021-10-26 19:44:45 +0300

    Enemy gonna  to the player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody enemyRb;
+    GameObject player;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.AddForce((player.transform
+            .position - transform.position).normalized * speed);
+    }
+}
